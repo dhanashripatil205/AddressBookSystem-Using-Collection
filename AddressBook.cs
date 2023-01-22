3b57a741bacab211719890d011c597c017686cff
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-22 15:52:46 +0530

    Updated!!!

--- AddressBook.cs
+++ AddressBook.cs
@@ -8,92 +8,6 @@ namespace CollectionAddressBook
 {
     public interface IAddressBookSystem
     {
-        void createUser();
-        void printUser();
-        void editContact();
-        void deleteContact();
-    }
-    public class AddressBook: IAddressBookSystem
-    {
-        public static LinkedList<Person> People = new LinkedList<Person>();
-        public void createUser()
-        {
-            Person person = new Person();
-
-            Console.Write("Enter First Name: ");
-            person.FirstName = Console.ReadLine();
-
-            Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
-
-            Console.Write("Enter Address : ");
-            person.Address = Console.ReadLine();
-
-            Console.Write("Enter City : ");
-            person.City = Console.ReadLine();
-
-            Console.Write("Enter State : ");
-            person.State = Console.ReadLine();
-
-            Console.Write("Enter ZipCode: ");
-            person.ZipCode = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNum = Console.ReadLine();
-
-            Console.Write("Enter EmailId: ");
-            person.EmailId = Console.ReadLine();
-
-            People.AddLast(person);
-        }
-        public void printUser()
-        {
-            if (People.Count == 0)
-            {
-                Console.WriteLine("Your address book is empty.");
-                Console.ReadKey();
-                return;
-            }
-            Console.WriteLine("\nHere are the current {0} people in your address book:\n", People.Count);
-            foreach (var person in People)
-            {
-                Console.WriteLine(" FirstName: {0},\n LastName: {1},\n Adress: {2},\n City : {3},\n State: {4},\n Zip: {5},\n PhoneNum: {6},\n Email: {7}", person.FirstName, person.LastName, person.Address, person.City, person.State, person.ZipCode, person.PhoneNum, person.EmailId);
-                Console.WriteLine("________________________________");
-            }
-        }
-        public  void editContact()
-        {
-            if (People.Count != 0)
-            {
-                Console.WriteLine("Enter the First Name of contact For editing details:");
-                string Modified = Console.ReadLine();
-                foreach (var person in People)
-                {
-                    if (person.FirstName.ToUpper() == Modified.ToUpper())
-                    {
-                        while (true)
-                        {
-                            Console.WriteLine("Which information do you want to Edit?");
-                            Console.WriteLine("#1: Firstname, #2: Lastname, 3#: Address, 4#: City, 5#: State, 6#: Zipcode, 7#: PhoneNum, 8#: EmailId, 9#: Exit");
-                            int userOption = Convert.ToInt32(Console.ReadLine()); 
-                            switch (userOption)
-                            {
-                                case 1:
-                                    Console.WriteLine("Enter the New First Name: ");
-                                    person.FirstName = Console.ReadLine();
-                                    break;
-                                case 2:
-                                    Console.WriteLine("Enter the New Last Name: ");
-                                    person.LastName = Console.ReadLine();
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Enter the New Address: ");
-                                    person.Address = Console.ReadLine();
-                                    break;                              
-                                case 4:
-                                    Console.WriteLine("Enter the New City: ");
-public interface IAddressBookSystem
-    {
       //  void createUser();
         void printUser();
         void editContact();
@@ -220,6 +134,67 @@ public interface IAddressBookSystem
                     Console.WriteLine("Contact is not present");
                 }
             }
+        }
+        public void searchperson()
+        {
+            Console.WriteLine("how you would like to search.");
+            Console.WriteLine("#1: using city\n #2: using state");
+            string inp = Console.ReadLine();
+          
+                switch (inp)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Enter city : ");
+                            string city = Console.ReadLine();
+                        foreach (var i in People)
+                        {
+                            Person forcity = People.Find(a => i.City.Equals(city));
+                            if (forcity == null)
+                            {
+                                Console.WriteLine("Match not fund");
+                            }
+                            else
+                            {
+                                Console.WriteLine("match found");
+
+                                // Console.WriteLine("Neme of person is {0} {1}.", i.FirstName, i.LastName);
+                                Console.WriteLine(i);
+                                
+                            }
+                        }
+                            break;
+                        }
+
+                    case "2":
+
+                        {
+                            Console.WriteLine("Enter state : ");
+                            string state = Console.ReadLine();
+                        foreach (var i in People)
+                        {
+                            Person forstate = People.Find(a => i.State.Equals(state));
+                            if (forstate == null)
+                            {
+                                Console.WriteLine("Match not fund");
+                            }
+                            else
+                            {
+                                Console.WriteLine("match found");
+                               
+                                    Console.WriteLine("Neme of person is {0} {1}.", i.FirstName, i.LastName);
+                                }
+                            }
+                            break;
+                        }
+
+                    default:
+                        { 
+                            Console.WriteLine("Enter valid input!");
+                           
+                            break;
+                        }
+                }
         }
     }
 }
