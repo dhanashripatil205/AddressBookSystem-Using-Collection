d925e3b1ac8d4ffa057d16323463db2c2c30300b
dhanashripatil205 <107808336+dhanashripatil205@users.noreply.github.com>
2023-01-22 15:59:18 +0530

    Updated!!!

--- AddressBook.cs
+++ AddressBook.cs
@@ -156,10 +156,13 @@ namespace CollectionAddressBook
                             }
                             else
                             {
-                                Console.WriteLine("match found");
+                                Console.WriteLine("match found\n");
 
-                                // Console.WriteLine("Neme of person is {0} {1}.", i.FirstName, i.LastName);
-                                Console.WriteLine(i);
+                                Console.WriteLine("Neme of person is {0} {1}.\n", i.FirstName, i.LastName);
+                                Console.WriteLine("Details of person\n");
+                                Console.WriteLine(" FirstName: {0},\n LastName: {1},\n Adress: {2},\n City : {3},\n State: {4},\n Zip: {5},\n PhoneNum: {6},\n Email: {7}", i.FirstName, i.LastName, i.Address, i.City, i.State, i.ZipCode, i.PhoneNum, i.EmailId);
+                                Console.WriteLine("-----------------------------------------------------------------");
+                                Console.WriteLine();
                                 
                             }
                         }
@@ -180,18 +183,21 @@ namespace CollectionAddressBook
                             }
                             else
                             {
-                                Console.WriteLine("match found");
+                                Console.WriteLine("match found\n");
                                
-                                    Console.WriteLine("Neme of person is {0} {1}.", i.FirstName, i.LastName);
-                                }
+                                    Console.WriteLine("Neme of person is {0} {1}.", i.FirstName, i.LastName+"\n");
+                                Console.WriteLine("Details of person\n");
+                                Console.WriteLine(" FirstName: {0},\n LastName: {1},\n Adress: {2},\n City : {3},\n State: {4},\n Zip: {5},\n PhoneNum: {6},\n Email: {7}", i.FirstName, i.LastName, i.Address, i.City, i.State, i.ZipCode, i.PhoneNum, i.EmailId);
+                                Console.WriteLine("-----------------------------------------------------------------");
+                                Console.WriteLine();
+                            }
                             }
                             break;
                         }
 
                     default:
                         { 
-                            Console.WriteLine("Enter valid input!");
-                           
+                            Console.WriteLine("Enter valid input!");                        
                             break;
                         }
                 }
